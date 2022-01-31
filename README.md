2a455fdaffb134f526c581fc4a830c66ec0b8978
Jimmy Rando <jsturat46925@gmail.com>
2022-01-31 17:36:28 -0500

    added potion code using tdd and jest

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # jest-another-RPG
+
+## Description
+The purpose of the project is to create a game using the commend line. The reason for
+this approach is to allow testing to be done to the project.
+
+## Project Cotents
+
+
+### Additional Libraries
+
+
+# Contributors
+Made by Michael Brown (aka randomguy1234)
---
+++ __tests__/Potion.test.js
@@ -0,0 +1,18 @@
+const Potion= require('../lib/Potion.js');
+
+test('creates a heatlh potion object', () => 
+{
+    const potion= new Potion('health');
+
+    expect(potion.name).toEqual('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random health object', () => 
+{
+    const potion= new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));   
+});
