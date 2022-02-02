372f6802d2880caaf9fbf42efae5e997f364d59a
Jimmy Rando <jsturat46925@gmail.com>
2022-02-02 15:24:15 -0500

    player has inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,7 +3,7 @@ const Potion= require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+//console.log(new Potion());
 
 
 test('creates a player object', () => 
@@ -15,4 +15,26 @@ test('creates a player object', () =>
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+
+test("gets player's stats as an object", () => 
+{
+    const player= new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () =>
+{
+    const player= new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory= [];
+
+    expect(player.getInventory()).toEqual(false);
 });
